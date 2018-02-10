3874cfb8f8175f4e23d6b6dafe56c25de853d029
rcretu <rcretu@sfu.ca>
2018-02-10 14:51:25 -0800

    Add index.js skeleton

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import Web3 from 'web3'
+import './../css/index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
